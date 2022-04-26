a50b1a7ce61f028f06c296b38aa419657525a308
alcortes <alejandro.alfa3037@gmail.com>
2022-04-26 10:25:14 -0500

    Creación de prueba e implementación de clase Reader

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,12 @@
+const Reader = require('../../../lib/utils/Reader.js')
+
+describe('Prueba de método de lectura de archivo JSON', () => {
+  test('Método readJsonFile', () => {
+    let resultado = Reader.readJsonFile('./test/lib/utils/prueba.json')
+    console.log(resultado)
+    expect(resultado).toMatchObject([
+      {data: 'correcto'},
+    ])
+  })
+
+})
