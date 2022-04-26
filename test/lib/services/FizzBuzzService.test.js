dae3ca6e6cfbbad9c283fb1db85d16e3a0dbdfdc
alcortes <alejandro.alfa3037@gmail.com>
2022-04-26 11:52:31 -0500

    Agregando pruebas e implementación de FizzbizzService

---
+++ test/lib/services/FizzBuzzService.test.js
@@ -0,0 +1,30 @@
+const FizzBuzz = require('../../../lib/services/FizzbuzzService.js')
+
+const explorer1 = {name: "Explorer1", score: 1}
+const explorer2 = {name: "Explorer1", score: 3}
+const explorer3 = {name: "Explorer1", score: 5}
+const explorer4 = {name: "Explorer1", score: 15}
+
+describe('Pruebas para evaluar la funcionalidad FizzBuzz', () => {
+  test('Validación para valores 3',() => {
+    FizzBuzz.applyValidationInExplorer(explorer2)
+    expect(explorer2.trick).toBe("FIZZ")
+  })
+
+  test('Validación para valores 5',() => {
+    FizzBuzz.applyValidationInExplorer(explorer3)
+    expect(explorer3.trick).toBe("BUZZ")
+  })
+
+  test('Validación para valores 15',() => {
+    FizzBuzz.applyValidationInExplorer(explorer4)
+    expect(explorer4.trick).toBe("FIZZBUZZ")
+  })
+
+  test('Validación para valores no divisibles por 3 y/o 5',() => {
+    FizzBuzz.applyValidationInExplorer(explorer1)
+    expect(explorer1.trick).toBe(explorer1.score)
+  })
+
+
+})
--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -3,7 +3,6 @@ const Reader = require('../../../lib/utils/Reader.js')
 describe('Prueba de método de lectura de archivo JSON', () => {
   test('Método readJsonFile', () => {
     let resultado = Reader.readJsonFile('./test/lib/utils/prueba.json')
-    console.log(resultado)
     expect(resultado).toMatchObject([
       {data: 'correcto'},
     ])
